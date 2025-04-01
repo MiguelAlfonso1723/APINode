aed2dad6773efa196dcf657aac26511d212eabd1
Miguel Angel Alfonso <105066751+MiguelAlfonso1723@users.noreply.github.com>
2025-03-31 22:10:52 -0500

    Implementacion Secretos 4
    Secretos

--- controllers/controller-product.js
+++ controllers/controller-product.js
@@ -8,7 +8,7 @@ import {
 import Company from "../models/company.mjs";
 import jwt from "jsonwebtoken";
 
-const key = process.env.SECRET;
+const key = secrets.SECRET;
 
 function validate(aux) {
   let token = aux;
